94d777fd74d807f9bca3df29df5363acc5b0425c
seppi <deet1022@gmail.com>
2016-02-02 22:13:20 +0900

    솔루션 추가

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld {
+	class Program {
+		static void Main(string[] args) {
+			int[] numbers = { 1023, 1023, 1034, 1087, 2034, 1111 };
+
+			bool result = !(numbers[2] < numbers[3]) && numbers[1] == numbers[0];
+
+			if (result) {
+				Console.WriteLine("같습니다");
+			} else {
+				Console.WriteLine("틀립니다");
+			}
+		}
+	}
+}
