ae2a50a6879f201ad9b80c3cf2b77cb7ced5c9c4
sepi <deet1022@gmail.com>
2016-02-04 00:45:04 +0900

    upgrade code

--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -3,15 +3,9 @@
 namespace HelloWorld {
 	class Program {
 		static void Main(string[] args) {
-			int[] numbers = { 1023, 1023, 1034, 1087 };
-
-			bool result = !(numbers[0] > numbers[2]) && numbers[0] == numbers[1] && numbers[1] <= numbers[3];
-
-			if (result) {
-				Console.WriteLine("맞습니다");
-			} else {
-				Console.WriteLine("틀립니다");
-			}
+			int num = 4;
+			string result = (num % 2 == 0) ? "even number" : "odd number";
+			Console.WriteLine(result);
 			//----디버그 모드에서 창이 바로 닫히는 문제를 위한 코드----
 			Console.ReadKey();
 		}
