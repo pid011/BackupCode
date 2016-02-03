0b6e5ea7b13e74a80cf02268a0512bf697a80772
sepi <deet1022@gmail.com>
2016-02-03 21:24:04 +0900

    코드 개선

--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -3,15 +3,17 @@
 namespace HelloWorld {
 	class Program {
 		static void Main(string[] args) {
-			int[] numbers = { 1023, 1023, 1034, 1087, 2034, 1111 };
+			int[] numbers = { 1023, 1023, 1034, 1087 };
 
-			bool result = !(numbers[2] < numbers[3]) && numbers[1] == numbers[0];
+			bool result = !(numbers[0] > numbers[2]) && numbers[0] == numbers[1] && numbers[1] <= numbers[3];
 
 			if (result) {
-				Console.WriteLine("같습니다");
+				Console.WriteLine("맞습니다");
 			} else {
 				Console.WriteLine("틀립니다");
 			}
+			//----디버그 모드에서 창이 바로 닫히는 문제를 위한 코드----
+			Console.ReadKey();
 		}
 	}
 }
